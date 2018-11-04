40657e4dd66e8c43194a35035953ed36b92ce027
ikijaem <ilija.malinovic@yahoo.com>
2018-11-04 18:26:49 +0100

    dodani repozitorijumi

---
+++ src/main/java/tim7/TIM7/repositories/LinijaRepository.java
@@ -0,0 +1,9 @@
+package tim7.TIM7.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tim7.TIM7.model.Linija;
+
+public interface LinijaRepository extends JpaRepository<Linija, Long> {
+
+}

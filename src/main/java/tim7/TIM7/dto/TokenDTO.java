13c99f811a5271aff250efa2959a4f5f180cc06f
draganaHrcek <dragana.hrcek@gmail.com>
2019-01-18 13:56:53 +0100

    Izmene login kontrolera zbog frontenda

---
+++ src/main/java/tim7/TIM7/dto/TokenDTO.java
@@ -0,0 +1,15 @@
+package tim7.TIM7.dto;
+
+public class TokenDTO {
+	String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+
+}

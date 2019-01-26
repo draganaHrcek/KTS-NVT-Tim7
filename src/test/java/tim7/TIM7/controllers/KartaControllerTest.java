3e69b14249337d9689a3eed756f4951186b28c4d
draganaHrcek <dragana.hrcek@gmail.com>
2019-01-26 18:28:46 +0100

    Jedinicni testovi KartaService

--- src/test/java/tim7/TIM7/controllers/KartaControllerTest.java
+++ src/test/java/tim7/TIM7/controllers/KartaControllerTest.java
@@ -1,5 +1,37 @@
-package tim7.TIM7.controllers;
-
-public class KartaControllerTest {
-
-}
+//package tim7.TIM7.controllers;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.web.context.WebApplicationContext;
+//
+//import tim7.TIM7.services.KartaService;
+//import tim7.TIM7.services.OsobaService;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+//public class KartaControllerTest {
+//	
+//	private static final String URL_PREFIX = "/karte";
+//	
+//
+//	private MockMvc mockMvc;
+//
+//	
+//	@Autowired
+//	private WebApplicationContext webApplicationContext;	
+//	
+//	@Autowired
+//	KartaService kartaServ;
+//	
+//	@Autowired
+//	KartaController kartaCon;
+//	
+//	
+//	
+//	
+//
+//}
